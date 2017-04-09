d321c5b27b0ba18c16c4f486e022ba44861691ab
Sairam <sairam.vj@gmail.com>
2017-04-09 13:02:42 +0530

    #Automate the deployment of MediaWiki on AWS cloud on CentOS

---
+++ lookup_plugins/aws_subnet_ids_from_names.py
@@ -0,0 +1,28 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import codecs
+
+from ansible.errors import *
+from ansible.plugins.lookup import LookupBase
+
+try:
+    import boto
+    import boto.vpc
+except ImportError:
+    raise AnsibleError("aws_subnet_ids_from_names lookup cannot be run without boto installed")
+
+class LookupModule(LookupBase):
+
+    def run(self, terms, variables=None, **kwargs):
+        if isinstance(terms, basestring):
+            terms = [terms]
+        subnet_ids = []
+        region = terms[0][0]
+        subnet_names = terms[0][1]
+        vpc_conn = boto.vpc.connect_to_region(region)
+        filters = {'tag:Name': terms[0][1]}
+        subnets = vpc_conn.get_all_subnets(filters=filters)
+        subnet_ids = [x.id.encode('utf-8') for x in subnets]
+        return subnet_ids
